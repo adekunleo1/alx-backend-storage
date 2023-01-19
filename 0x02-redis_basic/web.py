65707981c528695b015c221f1076296e911580f6
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-01-19 21:46:10 +0100

    web update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 """Implement an expiring web cache and tracker
 """
-
-
 import redis
 import requests
-rd = redis.Redis()
+r = redis.Redis()
 count = 0
 
 
 def get_page(url: str) -> str:
-    """track URL request and cache the result"""
-
-
-    rd.set(f"cached:{url}", count)
+    """track url request and cache the result"""
+    r.set(f"cached:{url}", count)
     resp = requests.get(url)
-    rd.incr(f"count:{url}")
-    rd.setex(f"cached:{url}", 10, rd.get(f"cached:{url}"))
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
     return resp.text
 
 
